8f6a19c2db66468e4dc83b7cb2057947fb0fd20c
Gabriel Ren <chicheng.ren1221@gmail.com>
2019-06-25 00:08:20 -0700

    fix #2 - fix cronjob for clearing player court number status

--- app/schedule/update_player.js
+++ app/schedule/update_player.js
@@ -13,11 +13,21 @@ module.exports = {
     // based on registered player, find reservations
     for (const player of registeredPlayers) {
       const reservation = await ctx.model.Reservation.findOne({ token: player.reservationToken });
+      let clearReservation = false;
 
-      // TODO: remove until bot migrate over
-      const endAt = reservation.endAt ? reservation.endAt : reservation.startAt + 45 * 60 * 1000;
+      if (!reservation) {
+        // TODO: reservation might be cleared by bab-bot
+        clearReservation = true;
+      } else {
+        // TODO: remove until bot migrate over
+        const endAt = reservation.endAt ? reservation.endAt : reservation.startAt + 45 * 60 * 1000;
 
-      if (endAt < Date.now()) {
+        if (endAt < Date.now()) {
+          clearReservation = true;
+        }
+      }
+
+      if (clearReservation) {
         await ctx.model.Player.updateOne({ name: player.name }, { $unset: { reservationToken: '', courtNumber: '' } });
       }
     }
